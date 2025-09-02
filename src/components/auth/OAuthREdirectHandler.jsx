bc3454c462fa96ba979e368dd4da68231e4641d6
Yury Sinitskiy <unstable195@gmail.com>
2025-09-02 23:41:13 +0300

    update app

--- src/components/auth/OAuthREdirectHandler.jsx
+++ src/components/auth/OAuthREdirectHandler.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../tools/Loader';
 import apiFetch from '../services/apiFetch';
-import  RedirectByRole  from './RedirectByRole.js';
+import  RedirectByRole  from './RedirectByRole';
 import { getSession } from '../services/users/getSession';
 const OAuthRedirectHandler = () => {
   const navigate = useNavigate();
