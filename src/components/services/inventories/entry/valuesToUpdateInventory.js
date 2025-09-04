43ab6cf813efd656cf823adf32dbfe4a727d24aa
Yury Sinitskiy <unstable195@gmail.com>
2025-09-04 13:57:40 +0300

    Update valuesToUpdateInventory.js

--- src/components/services/inventories/entry/valuesToUpdateInventory.js
+++ src/components/services/inventories/entry/valuesToUpdateInventory.js
@@ -1,14 +1,14 @@
 const valuesToUpdateInventory = (valuesToUpdate, version) => {
   const dataToUpdate = {
-    title: valuesToUpdate.title,
-    description: valuesToUpdate.description,
-    imageUrl: valuesToUpdate.imageUrl,
-    isPublic: valuesToUpdate.isPublic,
+    //title: valuesToUpdate.title,
+    //description: valuesToUpdate.description,
+    //imageUrl: valuesToUpdate.imageUrl,
+    //isPublic: valuesToUpdate.isPublic,
     customIdFormat: valuesToUpdate.customIds,
-    category: valuesToUpdate.category,
-    tags: valuesToUpdate.tags,
+    //category: valuesToUpdate.category,
+    //tags: valuesToUpdate.tags,
     version: version,
-    users: valuesToUpdate.users,
+    //users: valuesToUpdate.users,
     fields: valuesToUpdate.fields.map((f) => ({
       slot: f.slot,
       title: f.title,
